c219fdb7d4559eced9c3f7f4aa771b6e7fa7c14a
Vivek Bhaskaran <vivek.bhaskaran@surveyanalytics.com>
2013-01-04 06:22:26 +0000

    fixed

--- setup.sh
+++ setup.sh
@@ -12,6 +12,6 @@ git config --global user.name "Vivek Bhaskaran"
 git config --global user.email vivek.bhaskaran@surveyanalytics.com
 
 
-cp rc.local /etc/
-cp .xinitrc ~pi/
+cp etc/rc.local /etc/
+cp pi/.xinitrc ~pi/
 chown pi:pi ~pi/.xinitrc
