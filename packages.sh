7d74dc727ec595f7b882f13124792a3bd51dec48
Vivek Bhaskaran <vivek.bhaskaran@surveyanalytics.com>
2013-01-06 01:50:06 -0800

    update pacged

--- packages.sh
+++ packages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-PACKAGES="git x11-xserver-utils exifprobe feh x11-apps emacs"
+PACKAGES="git x11-xserver-utils exifprobe feh x11-apps emacs apache2"
 
 apt-get update
 apt-get -y install $PACKAGES
