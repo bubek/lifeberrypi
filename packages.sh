4353af50a414bebcf2ea2f1746fb59f902f7ac7c
Vivek Bhaskaran <vivek.bhaskaran@surveyanalytics.com>
2014-08-09 03:14:08 +0000

    portmapper

--- packages.sh
+++ packages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-PACKAGES="git x11-xserver-utils exifprobe feh x11-apps emacs apache2"
+PACKAGES="git x11-xserver-utils exifprobe feh x11-apps emacs apache2 tcpdump portmapper"
 
 apt-get update
 apt-get -y install $PACKAGES
